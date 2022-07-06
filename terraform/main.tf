8694079c47302bbdc6ed9e0627a268c54d65a04d
gpr073 <lonewolf41320@gmail.com>
2022-07-06 14:00:16 +0530

    Adjustments

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,6 +58,11 @@ data "aws_ami" "amazon-linux" {
   owners = ["amazon"]
 }
 
+resource "aws_iam_instance_profile" "my_instance_profile" {
+    name = "my_instance_profile"
+    role = "${aws_iam_role.my_role_identifier}"
+}
+
 resource "aws_instance" "myapp-server" {
   ami = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
@@ -67,6 +72,7 @@ resource "aws_instance" "myapp-server" {
 
   associate_public_ip_address = true
   key_name = "newkey"
+  iam_instance_profile = "${aws_iam_instance_profile.my_instance_profile.name}"
 
   user_data = file("entry-script.sh")
 
